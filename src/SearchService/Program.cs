ced9b998e1a68606dee47e392ecb15604f5e8ef9
Ali Atyabi <aliatyabi63@gmail.com>
2024-12-10 10:06:10 +0330

    End of 26

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,9 +1,18 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -16,6 +25,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
 }
 
 app.Run();
